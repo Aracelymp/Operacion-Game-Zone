7138a74e4cfe536f08b0ece8e01771603f4d1e99
Dayan Millar <d.millar@duocuc.cl>
2026-09-08 14:07:38 -0300

    KOT-001 - Inicializar proyecto Game Zone

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "2.3.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
+tasks.test {
+    useJUnitPlatform()
+}

47701e0a2fbb656884afec3cdf105fe758617706
Dayan Millar <d.millar@duocuc.cl>
2026-09-08 16:19:46 -0300

    KOT-007 - Implementar tarifas polimorficas

--- src/main/kotlin/ConsolaVr.kt
+++ src/main/kotlin/ConsolaVr.kt
@@ -12,5 +12,14 @@ class ConsolaVr(
     modelo,
     tipoUsuario
 ) {
-    val tarifaBase: Double = 3000.0
+    override fun calcularTarifa(minutos: Int): Double {
+
+        val costoBase = (minutos / 60.0) * 3000.0
+
+        return if (accesoriosPremium) {
+            costoBase * 1.30
+        } else {
+            costoBase
+        }
+    }
 }
